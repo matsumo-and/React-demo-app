72cb28ad0dd01ac6cd3611934eda3bbf9f36e18e
matsumo-and <yh134.toisanda@gmail.com>
2022-05-08 02:48:07 +0900

    リファレンスをコピペ

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,33 @@ import './App.css';
 
 const test = String("this is test.");
 
+type Result<T,E extends Error> = Ok<T,E> | Err<T,E>;
+
+export class Ok<T,E extends Error> {
+  constructor(readonly val:T){}
+  isOk = (): this is Ok<T,E> => true;
+  isErr = (): this is Err<T,E> => false;
+}
+
+export class Err<T,E extends Error> {
+  constructor(readonly err:E){}
+  isOk = (): this is Ok<T,E> => false;
+  isErr = (): this is Err<T,E> => true;
+}
+
+export const withResult = <T, A extends any[], E extends Error>(
+  fn: (...args: A) => Promise<T>,
+  ) => async(...args: A): Promise<Result<T, E>> => {
+  try{
+    return new Ok(await fn(...args));
+  }catch(error){
+    if (error instanceof Error){
+      return new Err(error as E);
+    }
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
