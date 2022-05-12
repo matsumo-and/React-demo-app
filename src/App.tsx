8a22fef59bce8399d72a8d82a6de1561856c8ab8
matsumo-and <yh134.toisanda@gmail.com>
2022-05-12 23:00:56 +0900

    form練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Console, debug } from 'console';
 
-const test = String("this is test.");
+let test:string = "";
 
 type Result<T,E extends Error> = Ok<T,E> | Err<T,E>;
 
@@ -18,6 +19,7 @@ export class Err<T,E extends Error> {
   isErr = (): this is Err<T,E> => true;
 }
 
+/*
 export const withResult = <T, A extends any[], E extends Error>(
   fn: (...args: A) => Promise<T>,
   ) => async(...args: A): Promise<Result<T, E>> => {
@@ -29,7 +31,7 @@ export const withResult = <T, A extends any[], E extends Error>(
     }
   }
 }
-
+*/
 
 function App() {
   return (
@@ -40,6 +42,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         </div>
+        <div>
+          <Testapp/>
+        <ValueWithOnChange/>
+        </div>
         <form>
           <label>
             Name:
@@ -47,6 +53,15 @@ function App() {
           </label>
           <input type="submit" value="Submit" />
         </form>
+        <textarea>
+          this is sample
+        </textarea>
+        <select>
+        <option value="grapefruit">Grapefruit</option>
+        <option value="lime">Lime</option>
+        <option selected value="coconut">Coconut</option>
+        <option value="mango">Mango</option>
+        </select>
         <div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -64,6 +79,63 @@ function App() {
           Learn React more at
         </a>
       </header>
+    </div>
+  );
+}
+const ValueWithOnChange: React.FC = () => {
+  const [str, setStr] = useState("0123");
+  return (
+    <div>
+      <label>value: {test}</label>
+      <input
+        type="text"
+        value={str}
+        onChange={event =>{
+          setStr(event.target.value);
+          test = str;
+        }}
+      />
+    </div>
+  );
+};
+
+function Testapp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSend = (e:any) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2em' }}>
+      <h1>useStateでフォーム</h1>
+      <form onSubmit={handleSend}>
+        <div>
+          <label>
+            メールアドレス:
+            <input
+              name="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            パスワード：
+            <input
+              name="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+        <button
+        type="submit">
+          Submit
+          
+          </button>
+      </form>
     </div>
   );
 }
